ca47822fe1fe69375c1187b257c79895ac887642
Sacha Escudier <sacha.escudier@princeton.edu>
2024-11-30 17:09:19 -0500

    Updated mesh_visual.py with unstructured grid

--- fastfem/plotter/mesh_visual.py
+++ fastfem/plotter/mesh_visual.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pyvista as pv
-
+from pyvista import CellType
 
 class VisualMesh:
     def __init__(self, mesh):
@@ -11,58 +11,41 @@ class VisualMesh:
         Args:
             mesh: The mesh object.
         """
-        self.mesh = mesh[0]
-        self.mesh_type = mesh[0].type
+        self.mesh = mesh
 
 
-   
-
-
-    def plot_mesh(self, point_label: bool, colors) -> None:
+    def plot_mesh(self, point_label: bool, colors: str) -> None:
         """
         Plots the mesh
 
 
         Args:
             Color: List containing the colors of the mesh and edges, respectively.
-
+            point_label: Boolean value to determine whether the points are labeled or not.
 
         Returns:
             None
         """
+        # Defining the points and strips
+        points = np.array(list(self.mesh["surface"].mesh[0].nodes.values()))
+        strips = np.array(list(self.mesh["surface"].mesh[0].elements.values()))
 
+        # 0-indexing the strips
+        strips_flat = strips.ravel() - 1
+        cells = np.insert(strips_flat, np.arange(0, len(strips_flat), 3), 3)
+        cell_arr = np.array(cells, dtype=np.int32)
+        cell_types = np.full(len(strips), CellType.TRIANGLE, dtype=np.uint8) 
 
-        if self.mesh_type == "triangle":
-            number_of_nodes_per_element = 3
-        else:
-            number_of_nodes_per_element = 4
-
-
-        # Declaring coordinates of nodes
-        points = self.mesh.coordinates_of_nodes
-        points = points.reshape(len(points) // number_of_nodes_per_element, 3)
-
-
-        # Defining strips for the mesh
-        new_tags = np.array([i - 1 for i in self.mesh.nodes_of_elements])
-        cells = np.concatenate(([len(new_tags)], new_tags), dtype=int)
-
-
-        # Defining the mesh object for PyVista
-        mesh = pv.PolyData(points, strips=cells)
-
-
-
-
-        # Plotting
+        # Create the unstructured grid
+        grid = pv.UnstructuredGrid(cell_arr, cell_types, points)
         plotter = pv.Plotter()
-        plotter.add_mesh(mesh, show_edges=True, color="lightblue")
-        if point_label:
-            plotter.add_point_labels(mesh.points, range(mesh.n_points))
-        plotter.camera_position = 'yx'
+        plotter.add_mesh(grid, show_edges=True, color=colors)
+
+        points = grid.points
+        mask = points[:, 2] == 0 # Labeling points on xy plane
+        plotter.add_point_labels(points[mask], points[mask].tolist(), point_size=20, font_size=10)
+        plotter.camera_position = 'xy'
         plotter.show()
-
-
 
 
     def movie(self, resolution, fps, colors):
