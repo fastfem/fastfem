559cb5f88781c2f5ad071d4c4720f04e20c397c0
Sina Atalay <dev@atalay.biz>
2024-12-02 23:47:40 -0500

    fix linting issues

--- fastfem/plotter/mesh_visual.py
+++ fastfem/plotter/mesh_visual.py
@@ -14,10 +14,6 @@ class VisualMesh:
         self.mesh = mesh[0]
         self.mesh_type = mesh[0].type
 
-
-   
-
-
     def plot_mesh(self, point_label: bool, colors) -> None:
         """
         Plots the mesh
@@ -31,51 +27,35 @@ class VisualMesh:
             None
         """
 
-
         if self.mesh_type == "triangle":
             number_of_nodes_per_element = 3
         else:
             number_of_nodes_per_element = 4
 
-
         # Declaring coordinates of nodes
         points = self.mesh.coordinates_of_nodes
         points = points.reshape(len(points) // number_of_nodes_per_element, 3)
-
 
         # Defining strips for the mesh
         new_tags = np.array([i - 1 for i in self.mesh.nodes_of_elements])
         cells = np.concatenate(([len(new_tags)], new_tags), dtype=int)
 
-
         # Defining the mesh object for PyVista
         mesh = pv.PolyData(points, strips=cells)
-
-
-
 
         # Plotting
         plotter = pv.Plotter()
         plotter.add_mesh(mesh, show_edges=True, color="lightblue")
         if point_label:
             plotter.add_point_labels(mesh.points, range(mesh.n_points))
-        plotter.camera_position = 'yx'
+        plotter.camera_position = "yx"
         plotter.show()
-
-
-
 
     def movie(self, resolution, fps, colors):
         pass
 
-
-
-
     def gif(self, resolution):
         pass
-
-
-
 
     def save(self, file_name):
         pass
