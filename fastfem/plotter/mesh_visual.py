ea280c8f3897beb3163526253fbd59e00b85bc26
Sina Atalay <dev@atalay.biz>
2024-12-11 18:18:34 -0500

    fix

--- fastfem/plotter/mesh_visual.py
+++ fastfem/plotter/mesh_visual.py
@@ -273,7 +273,7 @@ class VisualMesh:
         # Creating file
         plotter.open_gif(filename)
         # plotter.show(auto_close=False)
-        plotter.write_frame()
+        # plotter.write_frame()
         text = None
 
         for i in range(1, frames):
